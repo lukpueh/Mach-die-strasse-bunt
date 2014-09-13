84740c21efc1a9cce1f3b095d34673acd5115d64
Lukas P <luk.puehringer@gmail.com>
2014-09-13 16:17:03 +0200

    sqlproblem

--- insert.sql
+++ insert.sql
@@ -1,33 +1,32 @@
-insert into images (f, n, file) values 
- (1, 19, 'F1_n19.png'),
- (2, 49, 'F2_n49.png'),
- (3, 49, 'F3_n49.png'),
- (4, 52, 'F4_n52.png'),
- (5, 53, 'F5_n53.png'),
- (6, 55, 'F6_n55.png'),
- (7, 57, 'F7_n57.png'),
- (8, 57, 'F8_n57.png'),
- (9, 59, 'F9_n59.png'),
-(10, 59, 'F10_n59.png'),
-(11, 59, 'F11_n59.png'),
-(12, 61, 'F12_n61.png'),
-(13, 63, 'F13_n63.png'),
-(14, 63, 'F14_n63.png'),
-(15, 63, 'F15_n63.png'),
-(16, 69, 'F16_n69.png'),
-(17, 69, 'F17_n69.png'),
-(18, 73, 'F18_n73.png'),
-(19, 88, 'F19_n88.png'),
-(20, 80, 'F20_n80.png'),
-(21, 80, 'F21_n80.png'),
-(22, 74, 'F22_n74.png'),
-(23, 74, 'F23_n74.png'),
-(24, 68, 'F24_n68.png'),
-(25, 73, 'F25_n73.png'),
-(26, 31, 'F26_n31.png'),
-(27, 89, 'F27_n89.png'),
-(29, 92, 'F29_n92.png'),
-(30, 88, 'F30_n88.png'),
-(31, 58, 'F31_n58.png'),
-(32, 52, 'F32_n52.png'),
-(34, 18, 'F34_n18.png');
+insert into images (f, n, file) values (1, 19, 'F1_n19.png');
+insert into images (f, n, file) values (2, 49, 'F2_n49.png');
+insert into images (f, n, file) values (3, 49, 'F3_n49.png');
+insert into images (f, n, file) values (4, 52, 'F4_n52.png');
+insert into images (f, n, file) values (5, 53, 'F5_n53.png');
+insert into images (f, n, file) values (6, 55, 'F6_n55.png');
+insert into images (f, n, file) values (7, 57, 'F7_n57.png');
+insert into images (f, n, file) values (8, 57, 'F8_n57.png');
+insert into images (f, n, file) values (9, 59, 'F9_n59.png');
+insert into images (f, n, file) values (10, 59, 'F10_n59.png');
+insert into images (f, n, file) values (11, 59, 'F11_n59.png');
+insert into images (f, n, file) values (12, 61, 'F12_n61.png');
+insert into images (f, n, file) values (13, 63, 'F13_n63.png');
+insert into images (f, n, file) values (14, 63, 'F14_n63.png');
+insert into images (f, n, file) values (15, 63, 'F15_n63.png');
+insert into images (f, n, file) values (16, 69, 'F16_n69.png');
+insert into images (f, n, file) values (17, 69, 'F17_n69.png');
+insert into images (f, n, file) values (18, 73, 'F18_n73.png');
+insert into images (f, n, file) values (19, 88, 'F19_n88.png');
+insert into images (f, n, file) values (20, 80, 'F20_n80.png');
+insert into images (f, n, file) values (21, 80, 'F21_n80.png');
+insert into images (f, n, file) values (22, 74, 'F22_n74.png');
+insert into images (f, n, file) values (23, 74, 'F23_n74.png');
+insert into images (f, n, file) values (24, 68, 'F24_n68.png');
+insert into images (f, n, file) values (25, 73, 'F25_n73.png');
+insert into images (f, n, file) values (26, 31, 'F26_n31.png');
+insert into images (f, n, file) values (27, 89, 'F27_n89.png');
+insert into images (f, n, file) values (29, 92, 'F29_n92.png');
+insert into images (f, n, file) values (30, 88, 'F30_n88.png');
+insert into images (f, n, file) values (31, 58, 'F31_n58.png');
+insert into images (f, n, file) values (32, 52, 'F32_n52.png');
+insert into images (f, n, file) values (34, 18, 'F34_n18.png');
